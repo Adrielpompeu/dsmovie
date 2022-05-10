6ba6c4e1cd847c8c2ba741b95a78f90f571f2367
Adrielpompeu <oadriel17@gmail.com>
2022-05-10 18:55:45 -0300

    Projeto Criado

---
+++ backend/src/main/java/com/superior/dsmovie/DsmovieApplication.java
@@ -0,0 +1,13 @@
+package com.superior.dsmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsmovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsmovieApplication.class, args);
+	}
+
+}
